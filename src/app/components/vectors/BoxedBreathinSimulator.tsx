1fb14dd7389e62bd94a3fea0ba7659a0af5228ed
mehdicharife <mehdi_charife@um5.ac.ma>
2025-12-20 12:34:33 +0100

    feat: timer reset

--- src/app/components/vectors/BoxedBreathinSimulator.tsx
+++ src/app/components/vectors/BoxedBreathinSimulator.tsx
@@ -20,22 +20,35 @@ const BoxedBreathinSimulator = () => {
 		count: (4 * 4 * breathingIntervalInSeconds) / 1000,
 	});
 	const [breathingInstruction, setBreathingInstruction] = useState("");
-	const [resetting, setResetting] = useState(false);
 
 	useEffect(() => {
+		if (localStorage.getItem("resetting") === "true") {
+			return;
+		}
 		if (
 			counter.count === 0 ||
 			counter.count === (4 * 4 * breathingIntervalInSeconds) / 1000
 		) {
+			if (counter.count === 0) {
+				setCounter({
+					count: (4 * 4 * breathingIntervalInSeconds) / 1000,
+				});
+			}
 			return;
 		}
 
 		setTimeout(() => {
+			if (localStorage.getItem("resetting") === "true") {
+				return;
+			}
 			setCounter({ count: counter.count - 1 });
 		}, 1000);
 	}, [counter]);
 
 	useEffect(() => {
+		if (localStorage.getItem("resetting") === "true") {
+			return;
+		}
 		if (breathingInstruction === "") {
 			return;
 		}
@@ -44,6 +57,9 @@ const BoxedBreathinSimulator = () => {
 			return;
 		}
 		setTimeout(() => {
+			if (localStorage.getItem("resetting") === "true") {
+				return;
+			}
 			setBreathingInstruction(
 				breathingInstruction === ""
 					? breathingInstructions[0]
@@ -59,6 +75,7 @@ const BoxedBreathinSimulator = () => {
 	}, [breathingInstruction]);
 
 	const handleStartClick = () => {
+		localStorage.setItem("resetting", "false");
 		if (!dotRef.current) {
 			return;
 		}
@@ -73,11 +90,7 @@ const BoxedBreathinSimulator = () => {
 	};
 
 	const handleReset = () => {
-		setResetting(true);
-
-		setTimeout(() => {
-			setResetting(false);
-		}, 100000);
+		localStorage.setItem("resetting", "true");
 
 		setTimeout(() => {
 			setCounter({
@@ -86,12 +99,11 @@ const BoxedBreathinSimulator = () => {
 			setBreathingInstruction("");
 			dotRef.current.style.left = 0;
 			dotRef.current.style.top = 0;
-		}, 1300);
+		}, 30);
 	};
 
 	const moveDot = (startTime: number) => {
-		console.log(resetting);
-		if (resetting) {
+		if (localStorage.getItem("resetting") === "true") {
 			return;
 		}
 
@@ -161,7 +173,7 @@ const BoxedBreathinSimulator = () => {
 	return (
 		<>
 			<div
-				className="border-2 border-blue-950 rounded-sm flex flex-col justify-center mb-8 py-8 relative"
+				className="w-[380px] border-2 border-blue-950 rounded-sm flex flex-col justify-center mb-8 py-8 relative "
 				ref={boxRef}
 			>
 				<div className="bg-[#005B70] text-white w-[110px] h-[110px] rounded-full flex items-center justify-center  m-[0_auto]">
@@ -173,29 +185,28 @@ const BoxedBreathinSimulator = () => {
 					ref={dotRef}
 					className="absolute block bg-blue-400 w-5 h-5 rounded-full top-0 left-0 -translate-x-1/2 -translate-y-1/2 z-50"
 				></span>
-				{counter.count !== 0 && (
+				{breathingInstruction !== "" && (
 					<div className="text-sm font-medium border-2 border-blue-800 py-1/2 px-3 rounded-sm absolute -top-4 -right-7 rotate-17 bg-white">
 						{timify(counter.count)}
 					</div>
 				)}
 			</div>
-			<div className="flex items-center justify-center gap-x-6 text-sm">
-				<button className="border-2 border-[#004E60] rounded-[2px] w-26 h-10 font-medium text-center cursor-pointer">
-					Pause
-				</button>
+			{breathingInstruction === "" && (
 				<button
 					onClick={handleStartClick}
-					className="bg-[#022932] text-white rounded-[2px] w-26 h-10 font-medium text-center cursor-pointer"
+					className="w-full bg-[#022932] text-white rounded-[2px] h-10 uppercase font-medium text-center cursor-pointer"
 				>
 					Start
 				</button>
+			)}
+			{breathingInstruction !== "" && (
 				<button
 					onClick={handleReset}
-					className="border-2 border-[#004E60] rounded-[2px] w-26 h-10 font-medium text-center cursor-pointer"
+					className="w-full border-2 border-[#004E60] rounded-[2px] h-10 uppercase font-medium text-center cursor-pointer"
 				>
 					Reset
 				</button>
-			</div>
+			)}
 		</>
 	);
 };
